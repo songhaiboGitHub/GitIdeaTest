4fee4b1fd664314b2ead371be36fc4bd34056534
songhaibo <594859595@qq.com>
2020-09-14 16:44:13 +0800

    重写Object类equals方法

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println( test());
+        System.out.println(new Main().equals("a"));
     }
     public static String test(){
         return "测试git";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
