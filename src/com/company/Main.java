6f3f9b9efae859c3cdcb9acdd162a8a3aca3ab24
songhaibo <594859595@qq.com>
2020-09-14 16:18:24 +0800

    增加测试方法

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println( test());
+    }
+    public static String test(){
+        return "测试git";
     }
 }
