1439daa12857a57b2a9bba8baca52a7cb08e18ce
songhaibo <594859595@qq.com>
2020-09-15 09:12:57 +0800

    增加一个GitClass

---
+++ src/com/company/com/company/GitClass.java
@@ -0,0 +1,13 @@
+package com.company.com.company;
+
+import sun.misc.PostVMInitHook;
+
+/**
+ * @author
+ * @version 创建时间：2020/9/15 9:11
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        
+    }
+}
